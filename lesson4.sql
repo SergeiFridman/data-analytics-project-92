a465aa2ad666df0b96cbb5c4f2a00fc65444f6ff
Sergei Fridman <fotojozh@gmail.com>
2025-09-15 11:32:04 +0300

    Удалил лишние файлы из репозитория

--- lesson4.sql
+++
@@ -1,4 +0,0 @@
-SELECT good_name AS goods FROM Payments
-INNER JOIN FamilyMembers ON Payments.family_member = FamilyMembers.member_id
-INNER JOIN Goods ON Payments.good = Goods.good_id
-WHERE FamilyMembers.status = 'son';
